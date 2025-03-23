7411d1f499f747297979e3bcd293119201b94757
Anand <anand271087.ak@gmail.com>
2025-03-23 07:47:06 +0530

    added cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from app.services import setup_routes
+from flask_cors import CORS 
 
 def create_app():
     app = Flask(__name__)
+    # Enable CORS for Netlify frontend
+    #CORS(app, origins=["https://symphonious-empanada-1fa992.netlify.app"])
+
+    # Or allow all origins temporarily (for testing)
+    CORS(app)
     setup_routes(app)
     return app
